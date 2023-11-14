8473ed231e62afb4185917c6cf6ab8f5f494faab
alistair-barfoot <104738937+alistair-barfoot@users.noreply.github.com>
2023-11-13 20:57:28 -0500

    should be good

--- lab4/chario.h
+++ lab4/chario.h
@@ -2,5 +2,9 @@
 #define _CHARIO_H_
 
 /* place character-I/O function prototypes here */
+extern void PrintChar(unsigned int input);
+extern void PrintString(char *str);
+extern void PrintHex(unsigned int input);
+extern void PrintHexString(char *hex);
 
 #endif /* _CHARIO_H_ */
