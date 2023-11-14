af18c6c63ceca8c626ccc0fa977f8223cdf98efb
alistair-barfoot <104738937+alistair-barfoot@users.noreply.github.com>
2023-11-13 21:09:34 -0500

    also should be working

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -1,10 +1,53 @@
 #include "nios2_control.h"
 
 /* place additional #define macros here */
-
+#define HEX_DISPLAY (volatile unsigned int *) 0x10000020
 
 /* define global program variables here */
+void ShowDigit (int display, int value){
+	int unsigned hex = 0;
+	switch(value){
+		default:
+			break;
+		case 0:
+			hex = 0x3F;
+			break;
+		case 1:
+			hex = 0x06;
+			break;
+		case 2:
+			hex = 0x5B;
+			break;
+		case 3:
+			hex = 0x4F;
+			break;
+		case 4:
+			hex = 0x66;
+			break;
+		case 5:
+			hex = 0x6D;
+			break;
+		case 6:
+			hex = 0x7D;
+			break;
+		case 7:
+			hex = 0x07;
+			break;
+		case 8:
+			hex = 0x7F;
+			break;
+		case 9:
+			hex = 0x67;
+			break;		
+	}
+	hex = hex << (display * 8);
 
+	unsigned int mask = 0xFF << (display * 8);
+	unsigned int curr_disp = *HEX_DISPLAY;
+
+	curr_disp = curr_disp & (curr_disp ^ mask);
+	*HEX_DISPLAY = curr_disp | hex;
+}
 
 /* place additional functions here */
 
