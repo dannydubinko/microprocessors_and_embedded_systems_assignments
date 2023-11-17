60840d2e9328b49d1b2d3f3105bc5d6cdf4b004e
alistair-barfoot <104738937+alistair-barfoot@users.noreply.github.com>
2023-11-16 19:00:35 -0500

    have the switching leds ?

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -2,8 +2,13 @@
 
 /* place additional #define macros here */
 #define HEX_DISPLAY (volatile unsigned int *) 0x10000020
+#define JTAG_UART_BASE ((volatile unsigned int *) 0x10001000)
+#define LEDS	((volatile unsigned int *) 0x10000010)
 
 /* define global program variables here */
+int timer_3_flag = 0;
+int timer_1_flag = 0;
+
 void ShowDigit (int display, int value){
 	int unsigned hex = 0;
 	switch(value){
@@ -62,10 +67,48 @@ void interrupt_handler(void)
 	unsigned int ipending;
 
 	/* read current value in ipending register */
+	ipending = NIOS2_READ_IPENDING();
 
 	/* do one or more checks for different sources using ipending value */
+	if ((ipending & 0b1) == 0b1) {
+		*TIMER_STATUS = *TIMER_STATUS & 0b10;
+		
+		//*LEDS = *LEDS ^ 0b1;
+		
+		
+	}
 
 	/* remember to clear interrupt sources */
+	if((ipending & 0x4000) == 0x4000) { // timer 1
+		*TIMER1_STATUS = *TIMER1_STATUS & 0b10;
+		temp = LEDS;
+		*temp = *temp ^ 0x707;
+	}
+	
+	if((ipending & 0x2000) == 0x2000) { // timer 3 - CYCLE the lEDS0
+		*TIMER3_STATUS = *TIMER3_STATUS & 0b10;
+		switch(timer_3_flag){
+			default:
+				break;
+			case 0:
+				*LEDS = 0x300;
+				break;
+			case 1:
+				*LEDS = 0x30;
+				break;
+			case 2:
+				*LEDS = 0x3;
+				break;
+			case 3:
+				*LEDS = 0x30;
+				break;	
+		}
+		if(timer_3_flag < 3){
+			timer_3_flag = timer_3_flag + 1;
+		} else {
+			timer_3_flag = 0;
+		}
+	}
 }
 
 /*-----------------------------------------------------------------*/
@@ -75,10 +118,20 @@ void Init (void)
 	/* initialize software variables */
 
 	/* set up each hardware interface */
+	*TIMER1_START_LO = 0x7840;
+	*TIMER1_START_HI = 0x017D;
+	*TIMER1_STATUS = 0b0;
+	*TIMER1_CONTROL = 0x7;
+
+	*TIMER3_START_LO = 0xBC20;
+	*TIMER3_START_HI = 0x00BE;
+	*TIMER3_STATUS = 0b0;
+	*TIMER3_CONTROL = 0x7;
 
 	/* set up ienable */
-
+	NIOS2_WRITE_STATUS(0b1);
 	/* enable global recognition of interrupts in procr. status reg. */
+	NIOS2_WRITE_IENABLE(0b11);
 }
 
 /*-----------------------------------------------------------------*/
